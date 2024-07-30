9a9901a41397654376ce181f99ac1608cb7d58c2
Serhii-Kysil <serik.kisel000@gmail.com>
2024-07-30 11:46:59 +0300

    connect basic routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,21 @@
 import "./App.css";
+import { Route, Routes, Navigate } from "react-router-dom";
+
+import HomePage from "../pages/HomePage/HomePage";
+import FavoritesPage from "../pages/FavoritesPage/FavoritesPage";
+import TeachersPage from "../pages/TeachersPage/TeachersPage";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/teachers" element={<TeachersPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
