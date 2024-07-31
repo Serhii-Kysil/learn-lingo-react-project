db4e950edcf5c6e325f3fde8aab21272da7f42fd
Serhii-Kysil <serik.kisel000@gmail.com>
2024-07-31 11:30:26 +0300

    fix key

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCFgoEUvMxoc_KyUlVWXuLtVFfgYS9bm1Q",
+  apiKey: "ad",
   authDomain: "learn-lingo-ad84f.firebaseapp.com",
   projectId: "learn-lingo-ad84f",
   storageBucket: "learn-lingo-ad84f.appspot.com",
