4a7504bf4c273794dfdfb921a80fa0849ef41549
Serhii-Kysil <serik.kisel000@gmail.com>
2024-07-31 10:07:50 +0300

    add welcome img

---
+++ src/components/WelcomeImg/WelcomeImg.jsx
@@ -0,0 +1,11 @@
+export const WelcomeImg = () => {
+  return (
+    <div>
+      <img
+        src="/public/main-img@1x-min.jpg"
+        alt="welcome-img"
+        srcSet="/public/main-img@2x-min.jpg"
+      />
+    </div>
+  );
+};
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,15 @@
+import { WelcomeImg } from "../../components/WelcomeImg/WelcomeImg";
 import { WelcomeInfo } from "../../components/WelcomeInfo/WelcomeInfo";
+
+import css from "./HomePage.module.css";
 
 export default function HomePage() {
   return (
-    <>
-      <WelcomeInfo />
-    </>
+    <div>
+      <div className={css.welcomeSection}>
+        <WelcomeInfo />
+        <WelcomeImg />
+      </div>
+    </div>
   );
 }
