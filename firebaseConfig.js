8e2237074cb76377fff61d823f507e4a226dddd2
Serhii-Kysil <serik.kisel000@gmail.com>
2024-07-31 11:25:55 +0300

    add firebaseConfig

---
+++ firebaseConfig.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCFgoEUvMxoc_KyUlVWXuLtVFfgYS9bm1Q",
+  authDomain: "learn-lingo-ad84f.firebaseapp.com",
+  projectId: "learn-lingo-ad84f",
+  storageBucket: "learn-lingo-ad84f.appspot.com",
+  messagingSenderId: "563516086482",
+  appId: "1:563516086482:web:3b9e109775cd18e30df50b",
+  measurementId: "G-R1XGV9NSP6",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const database = getDatabase(app);
+
+export { database };
