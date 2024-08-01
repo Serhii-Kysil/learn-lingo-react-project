3e11d4fc9d1097e71d3eb9736d5baeea87a6bf2a
Serhii-Kysil <serik.kisel000@gmail.com>
2024-08-01 13:57:07 +0300

    add modal close

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -3,17 +3,29 @@ import css from "./AuthNav.module.css";
 import { FiLogIn } from "react-icons/fi";
 
 import { SimpleModal } from "../SimpleModal/SimpleModal";
+import { useState } from "react";
 
 export const AuthNav = () => {
+  const [modalLogInIsOpen, setModalLogInOpen] = useState(false);
   return (
     <div className={css.wrapper}>
-      <button className={css.logBtn}>
+      <button className={css.logBtn} onClick={() => setModalLogInOpen(true)}>
         <FiLogIn className={css.icon} />
         Log in
       </button>
       <button className={css.regBtn}>Registration</button>
 
-      <SimpleModal />
+      <SimpleModal
+        isOpen={modalLogInIsOpen}
+        onClose={() => setModalLogInOpen(false)}
+      >
+        <h2>Log In</h2>
+        <p>
+          Welcome back! Please enter your credentials to access your account and
+          continue your search for an teacher.
+        </p>
+        <div>Form</div>
+      </SimpleModal>
     </div>
   );
 };
--- src/components/SimpleModal/SimpleModal.jsx
+++ src/components/SimpleModal/SimpleModal.jsx
@@ -2,18 +2,28 @@ import css from "./SimpleModal.module.css";
 
 import { IoMdClose } from "react-icons/io";
 
-export const SimpleModal = () => {
+export const SimpleModal = ({ isOpen, onClose, children }) => {
+  const onWrapperClick = (event) => {
+    if (event.target.classList.contains("modal-wrapper")) onClose();
+  };
+
   return (
     <>
-      <div className={css.modal}>
-        <div className={css.modalWrapper}>
-          <div className={css.modalContent}>
-            <button className={css.closeBtn}>
-              <IoMdClose />
-            </button>
+      {isOpen && (
+        <div className={css.modal}>
+          <div
+            className={`${css.modalWrapper} modal-wrapper`}
+            onClick={onWrapperClick}
+          >
+            <div className={css.modalContent}>
+              <button className={css.closeBtn} onClick={() => onClose()}>
+                <IoMdClose className={css.icon} />
+              </button>
+              {children}
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
