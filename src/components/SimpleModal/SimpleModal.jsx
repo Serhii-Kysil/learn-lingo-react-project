bef94a2343a017c09485a1e32980698b5007b75f
Serhii-Kysil <serik.kisel000@gmail.com>
2024-08-02 09:34:22 +0300

    add modal closing on Esc

--- src/components/SimpleModal/SimpleModal.jsx
+++ src/components/SimpleModal/SimpleModal.jsx
@@ -2,10 +2,30 @@ import css from "./SimpleModal.module.css";
 
 import { IoMdClose } from "react-icons/io";
 
+import { useEffect } from "react";
+
 export const SimpleModal = ({ isOpen, onClose, children }) => {
   const onWrapperClick = (event) => {
     if (event.target.classList.contains("modal-wrapper")) onClose();
   };
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.key === "Escape") {
+        onClose();
+      }
+    };
+
+    if (isOpen) {
+      document.addEventListener("keydown", handleKeyDown);
+    } else {
+      document.removeEventListener("keydown", handleKeyDown);
+    }
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [isOpen, onClose]);
 
   return (
     <>
