286397cb43af8c7afd9ef082a57dde8e8035a7c8
Serhii-Kysil <serik.kisel000@gmail.com>
2024-08-02 10:45:17 +0300

    add avatars + names

--- src/components/TeachersList/TeachersList.jsx
+++ src/components/TeachersList/TeachersList.jsx
@@ -3,13 +3,15 @@ import { selectTeachers } from "../../redux/Teachers/selector";
 import { useSelector } from "react-redux";
 import { TeachersListItem } from "../TeachersListItem/TeachersListItem";
 
+import css from "./TeachersList.module.css";
+
 export const TeachersList = () => {
   const teachers = useSelector(selectTeachers);
   console.log(teachers);
 
   return (
     <>
-      <ul>
+      <ul className={css.techersList}>
         {teachers.map((teacher, index) => (
           <TeachersListItem teacher={teacher} key={index} />
         ))}
--- src/components/TeachersListItem/TeachersListItem.jsx
+++ src/components/TeachersListItem/TeachersListItem.jsx
@@ -1,7 +1,33 @@
+/* eslint-disable react/prop-types */
+import css from "./TeachersListItem.module.css";
+
+import { FiBookOpen } from "react-icons/fi";
+
 export const TeachersListItem = ({ teacher }) => {
   return (
-    <>
-      <li>{teacher.name}</li>
-    </>
+    <li className={css.listItem}>
+      <div className={css.imgCont}>
+        <img
+          src={teacher.avatar_url}
+          alt="teacher avatar"
+          className={css.img}
+        />
+      </div>
+      <div className={css.infoBlock}>
+        <div className={css.topBlock}>
+          <div className={css.nameBlock}>
+            <span className={css.span}>Languages</span>
+            <p className={css.name}>{`${teacher.name} ${teacher.surname} `}</p>
+          </div>
+          <div className={css.mainBar}>
+            <div className={css.lessonsFormat}>
+              <FiBookOpen className={css.bookIcon} />
+              <p className={css.format}>Lessons online</p>
+            </div>
+            <span className={css.line}></span>
+          </div>
+        </div>
+      </div>
+    </li>
   );
 };
