1697cc30a4863d7d3533c4c3ad3c17187c41b789
Serhii-Kysil <serik.kisel000@gmail.com>
2024-08-02 11:19:55 +0300

    fix car styling

--- src/components/TeachersListItem/TeachersListItem.jsx
+++ src/components/TeachersListItem/TeachersListItem.jsx
@@ -3,6 +3,7 @@ import css from "./TeachersListItem.module.css";
 
 import { FiBookOpen } from "react-icons/fi";
 import { IoStar } from "react-icons/io5";
+import { FaRegHeart } from "react-icons/fa";
 
 export const TeachersListItem = ({ teacher }) => {
   return (
@@ -20,31 +21,34 @@ export const TeachersListItem = ({ teacher }) => {
             <span className={css.span}>Languages</span>
             <p className={css.name}>{`${teacher.name} ${teacher.surname} `}</p>
           </div>
-          <div className={css.mainBar}>
-            <div className={css.lessonsFormat}>
-              <FiBookOpen className={css.bookIcon} />
-              <p className={css.format}>Lessons online</p>
+          <div className={css.topBar}>
+            <div className={css.mainBar}>
+              <div className={css.lessonsFormat}>
+                <FiBookOpen className={css.bookIcon} />
+                <p className={css.format}>Lessons online</p>
+              </div>
+
+              <span className={css.line}></span>
+
+              <p className={css.lessonsDone}>
+                Lessons done: {teacher.lessons_done}{" "}
+              </p>
+
+              <span className={css.line}></span>
+
+              <div className={css.ratingBlock}>
+                <IoStar className={css.starIcon} />
+                <p>Rating: {teacher.rating}</p>
+              </div>
+
+              <span className={css.line}></span>
+
+              <p className={css.price}>
+                Price / 1 hour:{" "}
+                <span className={css.priceSpan}>{teacher.price_per_hour}$</span>
+              </p>
             </div>
-
-            <span className={css.line}></span>
-
-            <p className={css.lessonsDone}>
-              Lessons done: {teacher.lessons_done}{" "}
-            </p>
-
-            <span className={css.line}></span>
-
-            <div className={css.ratingBlock}>
-              <IoStar className={css.starIcon} />
-              <p>Rating: {teacher.rating}</p>
-            </div>
-
-            <span className={css.line}></span>
-
-            <p className={css.price}>
-              Price / 1 hour:{" "}
-              <span className={css.priceSpan}>{teacher.price_per_hour}$</span>
-            </p>
+            <FaRegHeart className={css.heart} />
           </div>
         </div>
       </div>
