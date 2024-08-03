96bd5791a18f599857204a93ac04c864ab0b9931
Serhii-Kysil <serik.kisel000@gmail.com>
2024-08-03 14:08:52 +0300

    add levels markup

--- src/components/TeachersListItem/TeachersListItem.jsx
+++ src/components/TeachersListItem/TeachersListItem.jsx
@@ -77,6 +77,13 @@ export const TeachersListItem = ({ teacher }) => {
             Conditions:{" "}
             <span className={css.aboutSpan}>{teacher.conditions}</span>
           </p>
+          <div className={css.levelsBlock}>
+            {teacher.levels.map((level, index) => (
+              <span key={index} className={css.levelTag}>
+                #{level}
+              </span>
+            ))}
+          </div>
           {isExpanded && (
             <div className={css.expandedBlock}>
               <p className={css.expandedInfo}>{teacher.experience}</p>
