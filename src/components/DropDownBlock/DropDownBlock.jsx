dc248c4df966ae0f29f8951667fbc6ab29f1a573
Serhii-Kysil <serik.kisel000@gmail.com>
2024-08-06 13:16:05 +0300

    add dropBox styles

--- src/components/DropDownBlock/DropDownBlock.jsx
+++ src/components/DropDownBlock/DropDownBlock.jsx
@@ -1,6 +1,8 @@
 import { Box } from "@mui/material";
 import { useState } from "react";
 
+import css from "./DropDownBlock.module.css";
+
 import { LanguageDropdown } from "../LanguageDropdown/LanguageDropdown";
 import { KnowledgeLevelDropdown } from "../KnowledgeLevelDropdown/KnowledgeLevelDropdown";
 import { PriceDropdown } from "../PriceDropdown/PriceDropdown";
@@ -11,10 +13,19 @@ export const DropDownBlock = () => {
   const [price, setPrice] = useState(30);
 
   return (
-    <Box display="flex" justifyContent="space-around" p={3}>
-      <LanguageDropdown language={language} setLanguage={setLanguage} />
-      <KnowledgeLevelDropdown level={level} setLevel={setLevel} />
-      <PriceDropdown price={price} setPrice={setPrice} />
-    </Box>
+    <div className={css.container}>
+      <Box
+        display="flex"
+        justifyContent="space-around"
+        p={3}
+        gap={"20px"}
+        padding={0}
+        margin={"64px 0px 32px 0px"}
+      >
+        <LanguageDropdown language={language} setLanguage={setLanguage} />
+        <KnowledgeLevelDropdown level={level} setLevel={setLevel} />
+        <PriceDropdown price={price} setPrice={setPrice} />
+      </Box>
+    </div>
   );
 };
